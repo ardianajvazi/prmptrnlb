74e54d81a5251211453310bf830f991b38e92872
Brian Douglas <us42844@a34842-m.corp.premera.org>
2017-12-20 13:15:47 -0800

    test

--- src/app/Patterns/alerts/alerts-component.ts
+++ src/app/Patterns/alerts/alerts-component.ts
@@ -1,4 +1,14 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroupDirective, NgForm, Validators} from '@angular/forms';
+import {ErrorStateMatcher} from '@angular/material/core';
+
+/** Error when invalid control is dirty, touched, or submitted. */
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 
 @Component({
@@ -7,5 +17,10 @@ import { Component } from '@angular/core';
     styleUrls: ['./alerts-component.scss']
 })
 export class AlertsComponent {
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
 
+  matcher = new MyErrorStateMatcher();
 }
